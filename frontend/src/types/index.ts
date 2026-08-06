407ada4f6aa8152d4daed09f2f16b83b8b3aafcd
BATS SÉBASTIEN <sebastien.bats@gmail.com>
2026-08-06 11:17:46 +0200

    Create index.ts

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,6 @@
+export * from './fire.types';
+export * from './copernicus.types';
+export * from './meteo.types';
+export * from './sdis.types';
+export * from './map.types';
+export * from './api.types';
